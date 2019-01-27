3831a39fc427de4795bb69a31652b8cee3c15ed6
kyle mcloughlin <kylemcloughlin1000@hotmail.ca>
2019-01-27 16:31:20 +0000

    commiting practice looping exersices

---
+++ ingredients.js
@@ -0,0 +1,34 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+
+// Write a for loop that prints out the contents of ingredients:
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+
+var list = 0;
+
+console.log('Ingredients needed:');
+
+while (list < 5) { if (list === 100) { break;}
+console.log(ingredients[list]);
+list++;
+}
+
+
+
+
+
+
+
+
+
+console.log('Ingredients needed:');
+
+for (var i = 0; i < ingredients.length; i++){ console.log(ingredients[i]);
+}
+
+
+console.log('Ingredients needed:')
+
+for (var i = 5; i >  0; i--) {console.log(ingredients[i]); }
